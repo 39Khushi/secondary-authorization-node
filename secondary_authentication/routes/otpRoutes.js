bbff78c9653732fbde5aa5db55711c40c79a2609
39Khushi <skhushi392003@gmail.com>
2024-07-31 22:39:36 +0530

    Delete secondary_authentication directory

--- secondary_authentication/routes/otpRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const OtpController = require('../controllers/otpController');
-const router = express.Router();
-
-router.get('/get-otp', OtpController.getOtp);
-router.post('/validate-otp', OtpController.validateOtp);
-
-module.exports = router;
